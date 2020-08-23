c0d5fe881a2041c58845d9b69bb262bbd6b14838
Roberto Gomez <rob.77@live.com>
2020-08-23 15:08:19 -0500

    inital setup

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace Csharp_Prog
    {
       static void Main(string[] args)
       {
-         Console.WriteLine("Hello World!");
+         int count;
+         for (count = 1; count <= 10; ++count)
+         {
+            if (count == 5)
+            {
+               break;
+            }
+
+            Console.Write($"{count}  ");
+         }
+         Console.WriteLine($"\n Broke out of loop at count = {count}");
       }
    }
 }
