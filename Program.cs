5db12f9bc557adbeb6b745f2b6cdcfbce5c97bac
Roberto Gomez <rob.77@live.com>
2020-09-25 16:12:46 -0500

    410p

--- Program.cs
+++ Program.cs
@@ -4,19 +4,31 @@ namespace Csharp_Prog
 {
    class Program
    {
+
+      static void MyMethod()
+      {
+         int count = 0;
+         Console.WriteLine("I just got executed!");
+         Console.WriteLine("Enter a line");
+         string input = Console.ReadLine();
+
+         while (input != null)
+         {
+            count++;
+            Console.WriteLine("Enter a word");
+            input = Console.ReadLine();
+
+         }
+
+         Console.WriteLine(count);
+      }
+
+
+
       static void Main(string[] args)
       {
-         int count;
-         for (count = 1; count <= 10; ++count)
-         {
-            if (count == 5)
-            {
-               break;
-            }
+         MyMethod();
 
-            Console.Write($"{count}  ");
-         }
-         Console.WriteLine($"\n Broke out of loop at count = {count}");
       }
    }
 }
